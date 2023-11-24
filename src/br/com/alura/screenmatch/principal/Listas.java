30386771ba5c2316dd1b7e772c41badc3f20feff
Robervan Souza <103292038+RobervanSouza@users.noreply.github.com>
2023-11-24 09:18:40 -0300

    Criando Objetos

---
+++ src/br/com/alura/screenmatch/principal/Listas.java
@@ -0,0 +1,57 @@
+package br.com.alura.screenmatch.principal;
+
+import br.com.alura.screenmatch.modelos.Filme;
+import br.com.alura.screenmatch.modelos.Serie;
+import br.com.alura.screenmatch.modelos.Titulo;
+
+import java.util.ArrayList;
+
+public class Listas {
+    public static void main(String[] args) {
+        Filme meuFilme = new Filme("O poderoso chefão", 2012);
+        meuFilme.avalia(3);
+        Filme outroFilme = new Filme("Avatar", 1989);
+        meuFilme.avalia(6);
+        var filmeRobervan = new Filme("peratas do caribe!!!", 2000);
+        filmeRobervan.avalia(8);
+
+        Serie lost = new Serie("Lost", 2000);
+
+
+
+
+        ArrayList<Titulo> assistidos = new ArrayList<>();
+        assistidos.add(filmeRobervan);
+        assistidos.add(meuFilme);
+        assistidos.add(outroFilme);
+        assistidos.add(lost);
+
+        for (Titulo item: assistidos ) {
+            System.out.println(item.getNome());
+            if (item instanceof Filme filme && filme.getClassificacao() > 2){
+            System.out.println("classificação:  " + filme.getClassificacao());
+            };
+        }
+
+
+        Filme favorito = new Filme(" The Matrix", 1999);
+        favorito.avalia(10);
+
+        Filme outro = new Filme("teste", 2014);
+        outro.avalia(9);
+
+        Serie serie = new Serie("robervan", 2017);
+
+        ArrayList<Titulo> lista = new ArrayList<>();
+        lista.add(favorito);
+        lista.add(outro);
+        lista.add(serie);
+
+        for(Titulo item : lista) {
+            System.out.println("nome do filme:  " +item.getNome());
+            if (item instanceof Filme filme && filme.getClassificacao() > 2) {
+                System.out.println("Classificação do Filme:  " +filme.getClassificacao());
+            }
+        }
+    }
+}
--- src/br/com/alura/screenmatch/principal/Principal.java
+++ src/br/com/alura/screenmatch/principal/Principal.java
@@ -1,3 +1,5 @@
+package br.com.alura.screenmatch.principal;
+
 import br.com.alura.screenmatch.calculos.CalculadoraDeTempo;
 import br.com.alura.screenmatch.calculos.FiltroRecomendacao;
 import br.com.alura.screenmatch.modelos.Episodio;
@@ -8,8 +10,8 @@ import java.util.ArrayList;
 
 public class Principal {
     public static void main(String[] args) {
-        Filme meuFilme = new Filme();
-        meuFilme.setNome("O poderoso chefão");
+        Filme meuFilme = new Filme("O poderoso chefão", 2012);
+       // meuFilme.setNome(");
         meuFilme.setAnoDeLancamento(1970);
         meuFilme.setDuracaoEmMinutos(180);
         System.out.println("Duração do filme: " + meuFilme.getDuracaoEmMinutos());
@@ -26,17 +28,17 @@ public class Principal {
 
 
 
-        Serie lost = new Serie();
-        lost.setNome("Lost");
-        lost.setAnoDeLancamento(2000);
+        Serie lost = new Serie("Lost", 2000);
+//        lost.setNome("Lost");
+//        lost.setAnoDeLancamento(2000);
         lost.exibeFichaTecnica();
         lost.setTemporadas(10);
         lost.setEpisodiosPorTemporada(10);
         lost.setMinutosPorEpisodio(50);
         System.out.println("Duração para maratonar Lost: " + lost.getDuracaoEmMinutos());
 
-        Filme outroFilme = new Filme();
-        outroFilme.setNome("Avatar");
+        Filme outroFilme = new Filme("Avatar", 1989);
+        //outroFilme.setNome();
         outroFilme.setAnoDeLancamento(2023);
         outroFilme.setDuracaoEmMinutos(200);
 
@@ -55,8 +57,8 @@ public class Principal {
         episodio.setTotalVisualizacoes(300);
         filtro.filtra(episodio);
 
-        Filme filmeRobervan = new Filme();
-        filmeRobervan.setNome("peratas do caribe!!!");
+        Filme filmeRobervan = new Filme("peratas do caribe!!!", 2000);
+       // filmeRobervan.setNome();
         filmeRobervan.setDuracaoEmMinutos(200);
         filmeRobervan.setAnoDeLancamento(2003);
         filmeRobervan.avalia(10);
@@ -69,6 +71,7 @@ public class Principal {
         System.out.println("Primeiro Filme:  " + listaDeFilmes.get(0).getNome());
         System.out.println(listaDeFilmes);
         System.out.println("lista de filmes" + listaDeFilmes.get(0).toString());
+
 
     }
 }
